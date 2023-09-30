2cd2262606b69b824cbd1a097009b072f962d0e3
Anoop Rajan <anoopkrajan7@gmail.com>
2023-09-30 17:40:02 +0530

    assignment

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.driver;
+
+/**
+ *
+ * @author anoop
+ */
+public class Boat implements WaterVehicle {
+
+    String name;
+    int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
